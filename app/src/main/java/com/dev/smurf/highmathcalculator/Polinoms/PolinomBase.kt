2b5dc58aaf53f42597b124f8da4b83c008f62a05
Smurf <sudaru2@gmail.com>
2019-06-09 21:08:39 +0300

    Init

---
+++ app/src/main/java/com/dev/smurf/highmathcalculator/Polinoms/PolinomBase.kt
@@ -0,0 +1,9 @@
+package com.dev.smurf.highmathcalculator.Polinoms
+
+import com.example.smurf.mtarixcalc.complexNumber
+
+abstract class PolinomBase
+{
+    abstract fun root() : Array<complexNumber>
+    
+}
