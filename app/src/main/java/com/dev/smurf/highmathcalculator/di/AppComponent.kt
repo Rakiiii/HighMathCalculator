89d226dde0e8593e4b9a2ee9d7ca01a9dcf35c80
Smurf <sudaru2@gmail.com>
2019-07-09 14:50:55 +0300

    refactore AppComponnet

--- app/src/main/java/com/dev/smurf/highmathcalculator/di/AppComponent.kt
+++ app/src/main/java/com/dev/smurf/highmathcalculator/di/AppComponent.kt
@@ -6,14 +6,20 @@ import com.dev.smurf.highmathcalculator.mvp.presenters.SettingsPresenter
 import dagger.Component
 import javax.inject.Singleton
 
+
+//интрфейс для вставки зависимостей
 @Singleton
+//список зависимостей
 @Component(modules = [MatrixModule::class , PolinomModule::class , SettingsModule::class , MatrixDataBaseModule::class , PolinomDataBaseModule::class])
 interface AppComponent
 {
+    //вставка зависимостей в MatrixPresenter
     fun inject( matrixPresenter : MatrixPresenter )
 
+    //вставка зависимостей в PolinomPresenter
     fun inject( polinomPresenter: PolinomPresenter)
 
+    //вставка зависисмостей в SettingsPresenter
     fun inject( settingsPresenter: SettingsPresenter)
 
 }
