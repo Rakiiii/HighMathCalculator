e2b59809ee9215adbfa5973002a2baf5d810d7ed
Smurf <sudaru2@gmail.com>
2019-07-09 14:49:03 +0300

    refactore PolinomDataBase

--- app/src/main/java/com/dev/smurf/highmathcalculator/DataBase/PolinomDataBase.kt
+++ app/src/main/java/com/dev/smurf/highmathcalculator/DataBase/PolinomDataBase.kt
@@ -5,6 +5,7 @@ import androidx.room.RoomDatabase
 import com.example.smurf.mtarixcalc.PolinomGroup
 
 
+//получения интрфейса работы с бд
 @Database(entities = [PolinomGroup::class] , version = 1)
 abstract class PolinomDataBase : RoomDatabase()
 {
