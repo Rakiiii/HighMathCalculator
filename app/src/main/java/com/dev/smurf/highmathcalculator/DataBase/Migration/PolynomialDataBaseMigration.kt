079d9186499e57eeb4f5030d02c037c6a175144f
Smurf <sudaru2@gmail.com>
2020-05-28 23:34:55 +0300

    update db and imveiw for polynomials

--- app/src/main/java/com/dev/smurf/highmathcalculator/DataBase/Migration/PolynomialDataBaseMigration.kt
+++ app/src/main/java/com/dev/smurf/highmathcalculator/DataBase/Migration/PolynomialDataBaseMigration.kt
@@ -2,6 +2,7 @@ package com.dev.smurf.highmathcalculator.DataBase.Migration
 
 import androidx.room.migration.Migration
 import androidx.sqlite.db.SupportSQLiteDatabase
+import com.dev.smurf.highmathcalculator.Polynomials.PolynomialBase
 
 object PolynomialDataBaseMigration_2_3 : Migration(2,3)
 {
@@ -20,5 +21,43 @@ object PolynomialDataBaseMigration_2_3 : Migration(2,3)
         )
 
         database.execSQL("ALTER TABLE Tmp RENAME TO PolynomialGroup")
+    }
+}
+
+object PolynomialDataBaseMigration_3_4 : Migration(3,4)
+{
+    override fun migrate(database: SupportSQLiteDatabase)
+    {
+        val str = "\""+PolynomialBase.EmptyPolynomial.supString().substringBefore('@')+"\""
+        val change = "\"\""
+        database.execSQL(
+            "UPDATE PolynomialGroup Set polRightPolynomial = Replace(polRightPolynomial, $str , $change)"
+        )
+        database.execSQL(
+            "UPDATE PolynomialGroup Set polOstPolynomial = Replace(polOstPolynomial, $str , $change)"
+        )
+
+        database.execSQL(
+            "UPDATE PolynomialGroup Set polResPolynomial = Replace(polResPolynomial, $str , $change)"
+        )
+    }
+}
+
+object PolynomialDataBaseMigration_4_5 : Migration(4,5)
+{
+    override fun migrate(database: SupportSQLiteDatabase)
+    {
+        val str = "\"%@%\""
+        val change = "\"\""
+        database.execSQL(
+            "UPDATE PolynomialGroup Set polRightPolynomial = case when polRightPolynomial LIKE($str) then $change else polRightPolynomial end"
+        )
+        database.execSQL(
+            "UPDATE PolynomialGroup Set polOstPolynomial = case when polOstPolynomial LIKE($str)  then $change else polOstPolynomial end"
+        )
+
+        database.execSQL(
+            "UPDATE PolynomialGroup Set polResPolynomial = case when polResPolynomial LIKE($str) then $change else polResPolynomial end"
+        )
     }
 }
--- app/src/main/java/com/dev/smurf/highmathcalculator/Polynomials/PolynomialFactory.kt
+++ app/src/main/java/com/dev/smurf/highmathcalculator/Polynomials/PolynomialFactory.kt
@@ -4,7 +4,7 @@ class PolynomialFactory
 {
     fun createPolynomial(obj: String): PolynomialBase
     {
-        if(obj == "" || obj.substringBefore('@') == PolynomialBase.EmptyPolynomial.supString().substringBefore('@'))return PolynomialBase.EmptyPolynomial
+        if(obj == "" || obj == " " )return PolynomialBase.EmptyPolynomial
         return if (obj.contains('^')) ExponentialPolynomial.createExponentialPolynomial(obj)
         else LinearDiofantPolynomial.createLinearDiofantPolynomial(
             obj
